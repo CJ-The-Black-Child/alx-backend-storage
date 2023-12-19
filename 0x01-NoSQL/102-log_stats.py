21fed2a79d7466f249dea65846dd95a64fd968ae
CJ-The-Black-Child <congomigue@gmail.com>
2023-12-19 13:31:18 +0300

    Task 15: Python script to provide some stats about Nginx logs stored in MongoDB and adds the top 10 of the most present IPs in the collection nginx of the database logs

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Module for task 15 """
+from pymongo import MongoClient
+
+
+def print_nginx_request_logs(nginx_collection):
+    """ Prints stats about Nginx request logs """
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = nginx_collection.count_documents({'method': method})
+        print("\tmethod {}: {}".format(method, req_count))
+        status_checks_count = nginx_collection.count_documents(
+            {'method': 'GET', 'path': '/status'}
+        )
+        print("{} status check".format(status_checks_count))
+
+
+def print_top_ips(nginx_collection):
+    """ Prints statistics about the top 10 HTTP IPs in a collection."""
+    print("IPs:")
+    ips = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+    ])
+    for ip in ips:
+        print("\t{}: {}".format(ip["_id"], ip["count"]))
+
+
+if __name__ == "__main__":
+    """Provides some stats about Nginx logs stored in MongoDB."""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print_nginx_request_logs(nginx_collection)
+    print_top_ips(nginx_collection)
