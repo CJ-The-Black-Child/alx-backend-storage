f299849ce47969c0f234e21dc799fd528b4af8d2
CJ-The-Black-Child <congomigue@gmail.com>
2023-12-19 17:50:03 +0300

    checks with Alx checker debugging tools

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -5,16 +5,16 @@ from pymongo import MongoClient
 
 def print_nginx_request_logs(nginx_collection):
     """ Prints stats about Nginx request logs """
-    print("{} logs".format(nginx_collection.count_documents({})))
+    print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         req_count = nginx_collection.count_documents({'method': method})
         print("\tmethod {}: {}".format(method, req_count))
-        status_checks_count = nginx_collection.count_documents(
-            {'method': 'GET', 'path': '/status'}
-        )
-        print("{} status check".format(status_checks_count))
+    status_checks_count = nginx_collection.count_documents(
+        {'method': 'GET', 'path': '/status'}
+    )
+    print("{} status check".format(status_checks_count))
 
 
 def print_top_ips(nginx_collection):
